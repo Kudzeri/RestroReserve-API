e52ab9a51c8c024dd2e680c66e5891d4aec7fdf2
Kudzeri <kudzeri@gmail.com>
2024-12-04 20:25:27 +0500

    Add swagger documentation of API to frontend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const setupSwagger = require("./swagger/swagger");
 
 const routes = require("./routes/index").routes;
 
@@ -16,8 +17,9 @@ app.use(bodyParser.json());
 app.use("/api/auth", routes.authRoutes);
 app.use("/api/bookings", routes.bookingRoutes);
 
+setupSwagger(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту: ${PORT}`);
 });
-
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,9 +9,93 @@ const { registerSchema, loginSchema } = require("../validation/userValidation");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Маршруты для регистрации, авторизации и получения информации о пользователе
+ */
 
 router.post("/register", validationMiddleware(registerSchema), register);
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Регистрация нового пользователя
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Имя пользователя
+ *               email:
+ *                 type: string
+ *                 description: Адрес электронной почты
+ *               password:
+ *                 type: string
+ *                 description: Пароль пользователя
+ *               phone:
+ *                 type: string
+ *                 description: Телефонный номер
+ *     responses:
+ *       201:
+ *         description: Пользователь успешно зарегистрирован
+ *       400:
+ *         description: Пользователь уже существует
+ *       500:
+ *         description: Ошибка сервера
+ */
+
+
 router.post("/login", validationMiddleware(loginSchema), login);
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Авторизация пользователя
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Успешная авторизация
+ *       404:
+ *         description: Неверный email или пароль
+ *       500:
+ *         description: Ошибка сервера
+ */
+
+
 router.get("/me", authMiddleware, getCurrentUser); 
+/**
+ * @swagger
+ * /api/auth/me:
+ *   get:
+ *     summary: Получить данные текущего пользователя
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Успешный ответ с данными пользователя
+ *       404:
+ *         description: Пользователь не найден
+ *       500:
+ *         description: Ошибка сервера
+ */
 
 module.exports = router;
